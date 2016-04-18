23215ff6ec58cd6e6bd713f86b13aa90887ef847
laurenn <laurenn.lam@kitware.com>
2016-04-18 18:26:34 +0200

    Cuda add macro for debugging

--- CudaReconstruction.cu
+++ CudaReconstruction.cu
@@ -6,6 +6,16 @@ __global__ void kernel(int *a, int *b)
   a[threadIdx.x] = b[threadIdx.x];
 }
 
+#define CudaErrorCheck(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true)
+{
+  if (code != cudaSuccess)
+  {
+    fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+    if (abort) exit(code);
+  }
+}
+
 int cuda_reconstruction(
     double h_gridMatrix[16], double h_gridOrig[3], int h_gridDims[3], double h_gridSpacing[3],
     int h_depthMapDims[3], double* h_depths, double h_depthMapMatrixK[16], double h_depthMapMatrixTR[16],
@@ -21,8 +31,8 @@ int cuda_reconstruction(
   int *d_a, *d_b;
   cudaMalloc((void**)&d_a, N * sizeof(int));
   cudaMalloc((void**)&d_b, N * sizeof(int));
-  cudaMemcpy(d_a, h_a, N * sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b, h_b, N * sizeof(int), cudaMemcpyHostToDevice);
+  CudaErrorCheck(cudaMemcpy(d_a, h_a, N * sizeof(int), cudaMemcpyHostToDevice));
+  CudaErrorCheck(cudaMemcpy(d_b, h_b, N * sizeof(int), cudaMemcpyHostToDevice));
 
   // organize threads into blocks and grids
   dim3 dimBlock(N, 1, 1);
@@ -32,7 +42,7 @@ int cuda_reconstruction(
   kernel<<<dimGrid, dimBlock>>>(d_a, d_b);
 
   // transfer data from device to host
-  cudaMemcpy(h_a, d_a, N * sizeof(int), cudaMemcpyDeviceToHost);
+  CudaErrorCheck(cudaMemcpy(h_a, d_a, N * sizeof(int), cudaMemcpyDeviceToHost));
   for (int i = 0; i < N; i++)
   {
     h_outScalar[i] = h_a[i];
