a3831a233d3f2c5434c2cfcb43cfea5fb7f1517e
laurenn <laurenn.lam@kitware.com>
2016-04-21 11:30:21 +0200

    Check if vectors are orthogonals and clean main file

--- main.cxx
+++ main.cxx
@@ -27,6 +27,7 @@
 // OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "vtkImageData.h"
+#include "vtkMath.h"
 #include "vtkMatrix3x3.h"
 #include "vtkMatrix4x4.h"
 #include "vtkNew.h"
@@ -48,7 +49,9 @@
 
 #include <string>
 
-// read arguments
+//-----------------------------------------------------------------------------
+// READ ARGUMENTS
+//-----------------------------------------------------------------------------
 std::vector<int> g_gridDims;
 std::vector<double> g_gridSpacing;
 std::vector<double> g_gridOrigin;
@@ -63,48 +66,54 @@ double rayPotentialThick = 2; // Define parameter 'thick' on ray potential funct
 double rayPotentialRho = 3; // Define parameter 'rho' on ray potential function when cuda is using
 bool useCuda = true; // Determine if the algorithm reconstruction is launched on GPU (with cuda) or CPU (without cuda)
 
-// filled attributes
+//-----------------------------------------------------------------------------
+// FILLED ATTRIBUTES
+//-----------------------------------------------------------------------------
 std::vector<std::string> g_depthMapPathList; // Contains all depth map path
 std::vector<std::string> g_KRTPathList; // Contains all KRT matrix path
 std::vector<ReconstructionData*> g_dataList; // Contains all read depth map and matrix
 vtkMatrix4x4* g_gridMatrix;
 
-bool read_arguments(int argc, char ** argv);
-bool read_krtd(std::string filename, vtkMatrix3x3* matrixK, vtkMatrix4x4* matrixTR);
-bool createData();
-void createGridMatrix();
-std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems);
+//-----------------------------------------------------------------------------
+// FUNCTIONS
+//-----------------------------------------------------------------------------
+bool ReadArguments(int argc, char ** argv);
+bool AreVectorsOrthogonal();
+bool CreateReconstructionData();
+bool ReadKrtdFile(std::string filename, vtkMatrix3x3* matrixK, vtkMatrix4x4* matrixTR);
+void CreateGridMatrixFromInput();
+std::vector<std::string> &SplitString(const std::string &s, char delim, std::vector<std::string> &elems);
 
-// todo remove
-void init_arguments();
 
 //cudareconstruction.exe --gridDims 100 100 100 --gridSpacing 0.1 0.1 0.1 --gridOrigin -5 -5 -5 --gridVecX 1 0 0 --gridVecY 0 1 0 --gridVecZ 0 0 1 --dataFolder C:\Dev\nda\TRG\Data --outputGridFilename C:\Dev\nda\TRG\Data\output.vts
+//-----------------------------------------------------------------------------
+/* Main function */
 int main(int argc, char ** argv)
 {
-  // arguments
-  if (!read_arguments(argc, argv))
+  if (!ReadArguments(argc, argv))
     {
     return EXIT_FAILURE;
     }
 
   // Read and create a list of ReconstructionData
-  createData();
+  if (!CreateReconstructionData())
+    {
+    std::cerr << "Error during ReconstructionData construction" << std::endl;
+    return EXIT_FAILURE;
+    }
 
   // Create grid matrix from VecXYZ
-  createGridMatrix();
+  CreateGridMatrixFromInput();
 
-  // generate grid from arguments
+  // Generate grid from arguments
   vtkNew<vtkImageData> grid;
   grid->SetDimensions(&g_gridDims[0]);
   grid->SetSpacing(&g_gridSpacing[0]);
   grid->SetOrigin(&g_gridOrigin[0]);
 
 
-  // reconstruction
+  // Launch reconstruction process
   vtkNew<vtkCudaReconstructionFilter> cudaReconstructionFilter;
-  if (useCuda)
-    cudaReconstructionFilter->UseCudaOn();
-  else
     cudaReconstructionFilter->UseCudaOff();
   cudaReconstructionFilter->SetRayPotentialRho(rayPotentialRho);
   cudaReconstructionFilter->SetRayPotentialThickness(rayPotentialThick);
@@ -113,7 +122,6 @@ int main(int argc, char ** argv)
   cudaReconstructionFilter->SetGridMatrix(g_gridMatrix);
   cudaReconstructionFilter->Update();
 
-  // todo compute transform according to gridVecs
   vtkNew<vtkTransform> transform;
   transform->SetMatrix(g_gridMatrix);
   vtkNew<vtkTransformFilter> transformFilter;
@@ -132,75 +140,13 @@ int main(int argc, char ** argv)
   g_gridMatrix->Delete();
   g_dataList.clear();
 
+
   return EXIT_SUCCESS;
 }
 
-bool read_krtd(std::string filename, vtkMatrix3x3* matrixK, vtkMatrix4x4* matrixTR)
-{
-  // open the file
-  std::ifstream file(filename.c_str());
-  if (!file.is_open())
-    {
-    std::cerr << "Unable to open krtd file : " << filename  << std::endl;
-    return false;
-    }
-
-  std::string line;
-
-  // get matrix K
-  for (int i = 0; i < 3; i++)
-    {
-    getline(file, line);
-    std::istringstream iss(line);
-
-    for (int j = 0; j < 3; j++)
-      {
-      double value;
-      iss >> value;
-      matrixK->SetElement(i, j, value);
-      }
-    }
-
-  getline(file, line);
-
-  // get matrix R
-  for (int i = 0; i < 3; i++)
-    {
-    getline(file, line);
-    std::istringstream iss(line);
-
-    for (int j = 0; j < 3; j++)
-      {
-      double value;
-      iss >> value;
-      matrixTR->SetElement(i, j, value);
-      }
-    }
-
-  getline(file, line);
-
-  // get matrix T
-  getline(file, line);
-  std::istringstream iss(line);
-  for (int i = 0; i < 3; i++)
-    {
-    double value;
-    iss >> value;
-    matrixTR->SetElement(i, 3, value);
-    }
-
-  // finalize matrix TR
-  for (int j = 0; j < 4; j++)
-    {
-    matrixTR->SetElement(3, j, 0);
-    }
-  matrixTR->SetElement(3, 3, 1);
-
-  return true;
-}
-
 //-----------------------------------------------------------------------------
-bool read_arguments(int argc, char ** argv)
+/* Read input argument and check if they are valid */
+bool ReadArguments(int argc, char ** argv)
 {
   bool help = false;
 
@@ -233,7 +179,13 @@ bool read_arguments(int argc, char ** argv)
   if (g_outputGridFilename == "" || g_depthMapContainer == "" || g_KRTContainer == "")
     {
     std::cerr << "Error arguments." << std::endl;
-    std::cerr << arg.GetHelp() ;
+    std::cerr << arg.GetHelp();
+    return false;
+    }
+
+  if (!AreVectorsOrthogonal())
+    {
+    std::cerr << "Given vectors are not orthogonals" << std::endl;
     return false;
     }
 
@@ -241,12 +193,33 @@ bool read_arguments(int argc, char ** argv)
 }
 
 //-----------------------------------------------------------------------------
-bool createData()
+/* Check if input vectors are orthogonals (gridVecX, gridVecY, gridVecZ) */
+bool AreVectorsOrthogonal()
 {
+  double X[3] = { g_gridVecX[0], g_gridVecX[1], g_gridVecX[2] };
+  double Y[3] = { g_gridVecY[0], g_gridVecY[1], g_gridVecY[2] };
+  double Z[3] = { g_gridVecZ[0], g_gridVecZ[1], g_gridVecZ[2] };
+
+  double XY = vtkMath::Dot(X, Y);
+  double YZ = vtkMath::Dot(Y, Z);
+  double ZX = vtkMath::Dot(Z, X);
+
+  if (XY == 0 && YZ == 0 && ZX == 0)
+    return true;
+  return false;
+}
+
+//-----------------------------------------------------------------------------
+/* Read all depth map and matrix file to create a list of 'ReconstructionData'
+required to launch the process
+*/
+bool CreateReconstructionData()
+{
+
   std::string dmapGlobalFile = g_pathFolder + "\\" + g_depthMapContainer;
   std::string krtGlobalFile = g_pathFolder + "\\" + g_KRTContainer;
 
-  // open the file which contains depthMap path
+  // Open the files which contains depthMap and matrix files path
   std::ifstream depthMapContainer(dmapGlobalFile.c_str());
   std::ifstream matrixContainer(krtGlobalFile.c_str());
   if (!depthMapContainer.is_open() || !matrixContainer.is_open())
@@ -260,38 +233,48 @@ bool createData()
   std::string depthMapPath, matrixPath;
   while (!depthMapContainer.eof())
     {
+    // ----------------
     // DEPTH MAP
+    // ----------------
     std::getline(depthMapContainer, depthMapPath);
     // only get the file name, not the whole path
     std::vector <std::string> elems;
-    split(depthMapPath, '/', elems);
+    SplitString(depthMapPath, '/', elems);
+
+    // check if there are an empty line
     if (elems.size() == 0)
       {
       continue;
       }
+    // Create the real data path to access depth map file
     depthMapPath = g_pathFolder + "\\" + elems[elems.size() - 1];
 
+    // Read depth map file
     vtkXMLImageDataReader* depthMapReader = vtkXMLImageDataReader::New();
     depthMapReader->SetFileName(depthMapPath.c_str());
     depthMapReader->Update();
 
+    // ----------------
     // MATRIX
+    // ----------------
     std::getline(matrixContainer, matrixPath);
-    // only get the file name, not the whole path
+    // Only get the file name, not the whole path
     elems.clear();
-    split(matrixPath, '/', elems);
+    SplitString(matrixPath, '/', elems);
     matrixPath = g_pathFolder + "\\" + elems[elems.size() - 1];
 
     vtkMatrix3x3* depthMapMatrixK = vtkMatrix3x3::New();
     vtkMatrix4x4* depthMapMatrixTR = vtkMatrix4x4::New();
-    bool isReadOk = read_krtd(matrixPath, depthMapMatrixK, depthMapMatrixTR);
+    bool isReadOk = ReadKrtdFile(matrixPath, depthMapMatrixK, depthMapMatrixTR);
     // Skip the creation of a new data if matrix is not readable
     if (!isReadOk)
       {
       continue;
       }
 
+    // ----------------
     // CREATE DATA
+    // ----------------
     ReconstructionData* data = new ReconstructionData();
     data->SetDepthMap(depthMapReader->GetOutput());
     data->SetMatrixK(depthMapMatrixK);
@@ -300,67 +283,103 @@ bool createData()
     g_dataList.push_back(data);
     }
 
+  // If there is no enough data, don't launch process
+  if (g_dataList.size() == 0)
+    return false;
   return true;
 }
 
 //-----------------------------------------------------------------------------
-void createGridMatrix()
+/* Read .krtd file which contains 2 matrix */
+bool ReadKrtdFile(std::string filename, vtkMatrix3x3* matrixK, vtkMatrix4x4* matrixTR)
+{
+  // Open the file
+  std::ifstream file(filename.c_str());
+  if (!file.is_open())
+    {
+    std::cerr << "Unable to open krtd file : " << filename << std::endl;
+    return false;
+    }
+
+  std::string line;
+
+  // Get matrix K
+  for (int i = 0; i < 3; i++)
+  {
+    getline(file, line);
+    std::istringstream iss(line);
+
+    for (int j = 0; j < 3; j++)
+    {
+      double value;
+      iss >> value;
+      matrixK->SetElement(i, j, value);
+    }
+  }
+
+  getline(file, line);
+
+  // Get matrix R
+  for (int i = 0; i < 3; i++)
+  {
+    getline(file, line);
+    std::istringstream iss(line);
+
+    for (int j = 0; j < 3; j++)
+    {
+      double value;
+      iss >> value;
+      matrixTR->SetElement(i, j, value);
+    }
+  }
+
+  getline(file, line);
+
+  // Get matrix T
+  getline(file, line);
+  std::istringstream iss(line);
+  for (int i = 0; i < 3; i++)
+  {
+    double value;
+    iss >> value;
+    matrixTR->SetElement(i, 3, value);
+  }
+
+  // Finalize matrix TR
+  for (int j = 0; j < 4; j++)
+  {
+    matrixTR->SetElement(3, j, 0);
+  }
+  matrixTR->SetElement(3, 3, 1);
+
+  return true;
+}
+
+//-----------------------------------------------------------------------------
+/* Construct a vtkMatrix4x4 from grid vec X, Y and Z */
+void CreateGridMatrixFromInput()
 {
   vtkMatrix4x4* gridMatrix = vtkMatrix4x4::New();
   gridMatrix->Identity();
 
-  //// Fill matrix
-  //gridMatrix->SetElement(0, 0, g_gridVecX[0]);
-  //gridMatrix->SetElement(0, 1, g_gridVecX[1]);
-  //gridMatrix->SetElement(0, 2, g_gridVecX[2]);
-  //gridMatrix->SetElement(1, 0, g_gridVecY[0]);
-  //gridMatrix->SetElement(1, 1, g_gridVecY[1]);
-  //gridMatrix->SetElement(1, 2, g_gridVecY[2]);
-  //gridMatrix->SetElement(2, 0, g_gridVecZ[0]);
-  //gridMatrix->SetElement(2, 1, g_gridVecZ[1]);
-  //gridMatrix->SetElement(2, 2, g_gridVecZ[2]);
+  // Fill matrix
+  gridMatrix->SetElement(0, 0, g_gridVecX[0]);
+  gridMatrix->SetElement(0, 1, g_gridVecX[1]);
+  gridMatrix->SetElement(0, 2, g_gridVecX[2]);
+  gridMatrix->SetElement(1, 0, g_gridVecY[0]);
+  gridMatrix->SetElement(1, 1, g_gridVecY[1]);
+  gridMatrix->SetElement(1, 2, g_gridVecY[2]);
+  gridMatrix->SetElement(2, 0, g_gridVecZ[0]);
+  gridMatrix->SetElement(2, 1, g_gridVecZ[1]);
+  gridMatrix->SetElement(2, 2, g_gridVecZ[2]);
 
   g_gridMatrix = gridMatrix;
 }
 
 //-----------------------------------------------------------------------------
-void init_arguments()
-{
-  g_gridDims.clear();
-  g_gridDims.push_back(100);
-  g_gridDims.push_back(100);
-  g_gridDims.push_back(100);
-
-  g_gridSpacing.clear();
-  g_gridSpacing.push_back(0.1);
-  g_gridSpacing.push_back(0.1);
-  g_gridSpacing.push_back(0.1);
-
-  g_gridOrigin.clear();
-  g_gridOrigin.push_back(-5);
-  g_gridOrigin.push_back(-5);
-  g_gridOrigin.push_back(-5);
-
-  g_gridVecX.clear();
-  g_gridVecX.push_back(1);
-  g_gridVecX.push_back(0);
-  g_gridVecX.push_back(0);
-
-  g_gridVecY.clear();
-  g_gridVecY.push_back(0);
-  g_gridVecY.push_back(1);
-  g_gridVecY.push_back(0);
-
-  g_gridVecZ.clear();
-  g_gridVecZ.push_back(0);
-  g_gridVecZ.push_back(0);
-  g_gridVecZ.push_back(1);
-
-  g_outputGridFilename = "C:/Dev/nda/TRG/CudaDepthMapIntegration/data/outputgrid.vts";
-}
-
-//-----------------------------------------------------------------------------
-std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems)
+/* Split a string from a delimiter char and return a vector of extracted words */
+std::vector<std::string> &SplitString(const std::string &s, char delim,
+                                      std::vector<std::string> &elems)
 {
   std::stringstream ss(s);
   std::string item;
