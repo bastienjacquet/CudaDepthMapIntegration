8432b25a97518e9e1c151ed0bbe5869f176015b8
laurenn <laurenn.lam@kitware.com>
2016-05-04 12:03:56 +0200

    [COLORATION] Make coloration on an external class instead if doing this on a main.cxx

--- Coloration/main.cxx
+++ Coloration/main.cxx
@@ -39,6 +39,7 @@
 #include "vtkXMLPolyDataReader.h"
 #include "vtkXMLPolyDataWriter.h"
 
+#include "MeshColoration.h"
 #include "Helper.h"
 #include "ReconstructionData.h"
 
@@ -77,135 +78,23 @@ int main(int argc, char ** argv)
   reader->SetFileName(g_inputPath.c_str());
   reader->Update();
 
+  MeshColoration* coloration = new MeshColoration(reader->GetOutput(), g_globalVTIFilePath, g_globalKRTDFilePath);
+  bool process = coloration->ProcessColoration();
 
-  ShowInformation("** Extract vti and krtd file path...");
-  std::vector<std::string> vtiList = help::ExtractAllFilePath(g_globalVTIFilePath.c_str());
-  std::vector<std::string> krtdList = help::ExtractAllFilePath(g_globalKRTDFilePath.c_str());
-  if (krtdList.size() < vtiList.size())
+  if (process)
     {
-    std::cerr << "Error, no enough krtd file for each vti file" << std::endl;
-    return EXIT_FAILURE;
+    ShowInformation("** Write output image");
+    vtkNew<vtkXMLPolyDataWriter> writer;
+    writer->SetFileName(g_outputPath.c_str());
+    writer->SetInputData(coloration->GetOutput());
+    writer->Update();
+    }
+  else
+    {
+    ShowInformation("Error during coloration process...");
     }
 
-  int nbDepthMap = (int)vtiList.size();
-
-  ShowInformation("** Read krtd and vti...");
-  std::vector<ReconstructionData*> dataList;
-  for (int id = 0; id < nbDepthMap; id++)
-    {
-    std::cout << "\r" << id * 100 / nbDepthMap << " %" << std::flush;
-    ReconstructionData* data = new ReconstructionData(vtiList[id], krtdList[id]);
-    dataList.push_back(data);
-    }
-
-  std::cout << "\r" << "100 %" << std::flush << std::endl << std::endl;
-  vtkPolyData* mesh = reader->GetOutput();
-  vtkPoints* meshPointList = mesh->GetPoints();
-  vtkIdType nbMeshPoint = meshPointList->GetNumberOfPoints();
-  //vtkIdType nbMeshPoint = 3;
-  int* depthMapDimensions = dataList[0]->GetDepthMap()->GetDimensions();
-
-  ShowInformation("** Process coloration for " + std::to_string(nbMeshPoint) + " points ...");
-
-
-  // Contains rgb values
-  vtkSmartPointer<vtkUnsignedCharArray> meanValues = vtkUnsignedCharArray::New();
-  meanValues->SetNumberOfComponents(3);
-  meanValues->SetNumberOfTuples(nbMeshPoint);
-  meanValues->FillComponent(0, 0);
-  meanValues->FillComponent(1, 0);
-  meanValues->FillComponent(2, 0);
-  meanValues->SetName("MeanColoration");
-
-  vtkSmartPointer<vtkUnsignedCharArray> medianValues = vtkUnsignedCharArray::New();
-  medianValues->SetNumberOfComponents(3);
-  medianValues->SetNumberOfTuples(nbMeshPoint);
-  medianValues->FillComponent(0, 0);
-  medianValues->FillComponent(1, 0);
-  medianValues->FillComponent(2, 0);
-  medianValues->SetName("MedianColoration");
-
-  vtkSmartPointer<vtkIntArray> projectedDMValue = vtkIntArray::New();
-  projectedDMValue->SetNumberOfComponents(1);
-  projectedDMValue->SetNumberOfTuples(nbMeshPoint);
-  projectedDMValue->FillComponent(0, 0);
-  projectedDMValue->SetName("NbProjectedDepthMap");
-
-  // Store each rgb value for each depth map
-  std::vector<double> list0;
-  std::vector<double> list1;
-  std::vector<double> list2;
-
-  for (vtkIdType id = 0; id < nbMeshPoint; id++)
-    {
-    std::cout << "\r" << id * 100 / nbMeshPoint << " %" << std::flush;
-
-    list0.reserve(nbDepthMap);
-    list1.reserve(nbDepthMap);
-    list2.reserve(nbDepthMap);
-
-    // Get mesh position from id
-    double position[3];
-    meshPointList->GetPoint(id, position);
-
-    for (int idData = 0; idData < nbDepthMap; idData++)
-      {
-      ReconstructionData* data = dataList[idData];
-
-      // Transform to pixel index
-      int pixelPosition[2];
-      data->TransformWorldToDepthMapPosition(position, pixelPosition);
-      // Test if pixel is inside depth map
-      if (pixelPosition[0] < 0 || pixelPosition[1] < 0 ||
-        pixelPosition[0] >= depthMapDimensions[0] ||
-        pixelPosition[1] >= depthMapDimensions[1])
-        {
-        continue;
-        }
-
-      double color[3];
-      data->GetColorValue(pixelPosition, color);
-
-      list0.push_back(color[0]);
-      list1.push_back(color[1]);
-      list2.push_back(color[2]);
-      }
-
-    // If we get elements
-    if (list0.size() != 0)
-      {
-      double sum0 = std::accumulate(list0.begin(), list0.end(), 0);
-      double sum1 = std::accumulate(list1.begin(), list1.end(), 0);
-      double sum2 = std::accumulate(list2.begin(), list2.end(), 0);
-      double nbVal = (double)list0.size();
-      meanValues->SetTuple3(id, sum0 / (double)nbVal, sum1 / (double)nbVal, sum2 / (double)nbVal);
-      double median0, median1, median2;
-      help::ComputeMedian<double>(list0, median0);
-      help::ComputeMedian<double>(list1, median1);
-      help::ComputeMedian<double>(list2, median2);
-      medianValues->SetTuple3(id, median0, median1, median2);
-      projectedDMValue->SetTuple1(id, list0.size());
-      }
-
-    list0.clear();
-    list1.clear();
-    list2.clear();
-    }
-
-  mesh->GetPointData()->AddArray(meanValues);
-  mesh->GetPointData()->AddArray(medianValues);
-  mesh->GetPointData()->AddArray(projectedDMValue);
-
-  std::cout << "\r" << "100 %" << std::flush << std::endl << std::endl;
-  ShowInformation("** Write output image");
-  vtkNew<vtkXMLPolyDataWriter> writer;
-  writer->SetFileName(g_outputPath.c_str());
-  writer->SetInputData(mesh);
-  writer->Update();
-
-  // Clean memory
-  for (int i = 0; i < dataList.size(); i++)
-    delete dataList[i];
+  delete coloration;
 
   return EXIT_SUCCESS;
 }
